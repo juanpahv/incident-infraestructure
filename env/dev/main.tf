52df1117b38581eac7028670ef251277bc1b064c
juanpahv <dcrunneryt1@gmail.com>
2024-10-14 20:01:35 +0900

    first commit

---
+++ env/dev/main.tf
@@ -0,0 +1,22 @@
+module "dev_vm" {
+  source = "../../modules/vm"
+  resource_group = "IN-RG-Dev"
+  security_group_name = "IN-SG-Dev"
+  vnet_name = "IN-VNET-Dev"
+  subnet_name = "IN-SUBNET-Dev"
+  public_ip_name = "IN-IP-Dev"
+  location = "eastus2"
+  admin_username = "adminuser"
+  nic_name = "IN-NIC-Dev"
+  vm_name = "IN-Server-Dev"
+  DOMAIN = var.DOMAIN
+  MAIL_SECRET_KEY = var.MAIL_SECRET_KEY
+  MAIL_SERVICE = var.MAIL_SERVICE
+  MAPBOX_ACCESS_TOKEN = var.MAPBOX_ACCESS_TOKEN
+  MONGO_INITDB_ROOT_USERNAME = var.MONGO_INITDB_ROOT_USERNAME
+  MONGO_INITDB_ROOT_PASSWORD = var.MONGO_INITDB_ROOT_PASSWORD
+  MONGO_DB = var.MONGO_DB
+  MAIL_USER = var.MAIL_USER
+  PORT = var.PORT
+  MONGO_URL = var.MONGO_URL
+}
